e1c27a9147a5ee9762be14006f085e9a36c60536
unteachable <unteachablegaming234@gmail.com>
2026-05-09 23:01:36 -0600

    updated coolpics

--- ponder/dom/dom.js
+++ ponder/dom/dom.js
@@ -4,29 +4,32 @@ selectElem.addEventListener('change', function(){
     console.log(codeValue);
 })
 
-//
-// let title = document.querySelector("h1"); // This targets the h1 element tag
-// console.log(title); // This prints the object to the console.
-//
-// title.textContent = "Web Page Components"; // This changes the text of the element.
-//
-// let topics = document.querySelector("#topics");
-// topics.style.color = "red"; // This changes every element of text in the topics div to the color red.
-//
-// const wrapper = document.getElementById("content");
-// wrapper.style.backgroundColor = "lightblue"; // This is the same as the color.
-//
-// let list = document.querySelector(".list"); // query will find the first element in the list.
-// list.style.border = "3px solid black"; // This is the same syntax as the CSS element string.
-//
-// let para = document.querySelector("p");
-// para.style.fontSize = "2em";
-// para.classList.add("background");
-//
-// const image = document.querySelector("img");
-// // This will change the src image source to a different image.
-// image.setAttribute("src", "images/new_logo.jpeg");
-//
+
+let title = document.querySelector("h1"); // This targets the h1 element tag
+console.log(title); // This prints the object to the console.
+
+title.textContent = "Web Page Components"; // This changes the text of the element.
+title.addEventListener("click", function () {
+    title.classList.toggle("highlight"); // this toggles the styles on the element off and on.
+});
+
+let topics = document.querySelector("#topics");
+topics.style.color = "red"; // This changes every element of text in the topics div to the color red.
+
+const wrapper = document.getElementById("content");
+wrapper.style.backgroundColor = "lightblue"; // This is the same as the color.
+
+let list = document.querySelector(".list"); // query will find the first element in the list.
+list.style.border = "3px solid black"; // This is the same syntax as the CSS element string.
+
+let para = document.querySelector("p");
+para.style.fontSize = "2em";
+para.classList.add("background");
+
+const image = document.querySelector("img");
+// This will change the src image source to a different image.
+image.setAttribute("src", "images/new_logo.jpeg");
+
 const dropDown = document.querySelector("#webdevlist");
 const html = document.querySelector("#html");
 const css = document.querySelector("#css");
@@ -41,3 +44,5 @@ dropDown.addEventListener("change", function() {
     let codeValue = dropDown.value; // This is getting one of the options in the dropdown
     document.getElementById(codeValue).style.color = "red";
 });
+
+// toggle
